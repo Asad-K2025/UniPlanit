394ceeb1506f54a2f22a3a370bf74f5f08fa0ff1
Asad Khalid <akha0107@uni.sydney.edu.au>
2025-06-26 09:34:14 +1000

    Semester marks calculatable and mobile viw improved

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.label import MDLabel
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDRectangleFlatButton, MDIconButton
@@ -16,9 +17,11 @@ class WidgetsUI(MDBoxLayout):  # Class needed to define the user interface as a 
 class MarksApp(MDApp):  # Class used to define backend logic
     def __init__(self):
         super().__init__()
-        self.subject_mark_dictionary = {}  # Dictionary used to store subject, mark and credit values
+        self.subjects_marks_dictionary = {}  # Dictionary used to store subject, mark and credit values
+        self.semester_labels_dictionary = {}  # Store labels to access them and change their values
+        self.semester_marks_sections_dictionary = {}  # Stores sections were semester marks are displayed
 
-    def build(self):  # Function defines main properties fo app
+    def build(self):  # Function defines main properties of app
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.theme_style = "Light"  # Main themes of app
         self.root = WidgetsUI()  # Passing widgets class as UI of this app (this allows easier referencing of widgets)
@@ -27,56 +30,92 @@ class MarksApp(MDApp):  # Class used to define backend logic
         self.on_window_resize(Window, Window.width, Window.height)  # Call once to initially set according to screen size
         return self.root
 
-    def on_window_resize(self, window, width, height):  # Adapt gpa output box according to screen size
-        if width < 400:
+    def on_window_resize(self, window, width, height):  # Adapt marks output box according to screen size
+        if width < 600:
             cols = 1
-        elif width < 700:
+        elif width < 900:
             cols = 2
         else:
             cols = 3
-        self.root.ids.gpa_output_box.cols = cols  # Setting columns of gpa output box to adapt to screen sizes
+        self.root.ids.degree_marks_output_box.cols = cols  # Setting columns of gpa output box to adapt to screen sizes
+        for semester in self.semester_marks_sections_dictionary.values():
+            semester.cols = cols
 
-    def init_dropdown_menu(self):  # Function creates the menu for the dropdown for selecting years with its options
+    def init_dropdown_menu(self):  # Function creates the menu for the dropdown for selecting semesters with its options
         options = [str(i) for i in range(1, 8 + 1)]  # Add option 1 to 8 in dropdown menu
         items = [{"text": opt, "viewclass": "OneLineListItem", "on_release": lambda x=opt: self.display_main_section(x)} for opt in options]
-        self.menu = MDDropdownMenu(caller=self.root.ids.year_dropdown, items=items, width_mult=4)  # Add menu to dropdown
+        self.menu = MDDropdownMenu(caller=self.root.ids.semester_dropdown, items=items, width_mult=4)  # Add menu to dropdown
 
-    def display_main_section(self, years_in_degree):  # Function displays current_section with subject, mark and credit based on dropdown
-        self.root.ids.year_dropdown.set_item(years_in_degree)  # Set value of dropdown to selected value from menu
+    def display_main_section(self, semesters_in_degree):  # Function displays current_section with subject, mark and credit based on dropdown
+        self.root.ids.semester_dropdown.set_item(semesters_in_degree)  # Set value of dropdown to selected value from menu
         self.menu.dismiss()  # Close the menu after number was selected
         self.root.ids.scroll_container.clear_widgets()  # Empty out scroll container in case of previous selection
-        self.subject_mark_dictionary.clear()  # Clear dictionary as widgets have been reset after year selection
+        self.subjects_marks_dictionary.clear()  # Clear dictionary as widgets have been reset after semester selection
 
-        for year in range(1, int(years_in_degree) + 1):
-            section_for_year = MDBoxLayout(orientation="vertical", spacing=dp(10), size_hint_y=None)
-            section_for_year.bind(minimum_height=section_for_year.setter("height"))  # Set height of this layout to its contents height
+        for semester in range(1, int(semesters_in_degree) + 1):
+            section_for_semester = MDBoxLayout(orientation="vertical", spacing=dp(10), size_hint_y=None)
+            section_for_semester.bind(minimum_height=section_for_semester.setter("height"))  # Set height of this layout to its contents height
 
-            year_label = f"Year {year}"  # Labels for Year 1 to Last Year
-            section_for_year.add_widget(MDLabel(text=year_label, font_style="H6", theme_text_color="Primary"))
-
-            section_for_year.add_widget(MDLabel(height=dp(1)))  # Add gap between year title and widgets
+            semester_label = f"Semester {semester}"  # Labels for semester 1 to Last semester
+            section_for_semester.add_widget(MDLabel(text=semester_label, font_style="H6", theme_text_color="Primary"))
+            section_for_semester.add_widget(MDLabel(height=dp(1)))  # Add gap between semester title and widgets
 
             rows = []
-            for _ in range(4):  # Creates 4 subjects for each year in a box layout representing each year
-                row = self.create_subject_row(section_for_year, year_label)
-                section_for_year.add_widget(row)
+            for _ in range(4):  # Creates 4 subjects for each semester in a box layout representing each semester
+                row = self.create_subject_row(section_for_semester, semester_label)
+                section_for_semester.add_widget(row)
                 rows.append(row)
 
-            self.subject_mark_dictionary[year_label] = rows  # Initialise dictionary with year labels to use
+            self.subjects_marks_dictionary[semester_label] = rows  # Initialise dictionary with semester labels to use
 
-            add_btn = MDRectangleFlatButton(  # Button to add subject to each year
+            section_for_semester.add_widget(MDLabel(height=dp(1)))  # Add gap between widgets and semester marks section
+            semester_marks_section = MDGridLayout(
+                cols=3,  # Used to adjust for mobile
+                size_hint_x=1,
+                size_hint_y=None,
+                adaptive_height=True,
+                spacing=dp(10),
+                padding=dp(10)
+            )  # Section showcasing semester marks
+            self.semester_marks_sections_dictionary[semester_label] = semester_marks_section  # Add section to dict to track
+
+            section_for_semester.add_widget(MDLabel(text="[b]Semester marks summary:[/b]",
+                                                    theme_text_color="Primary",
+                                                    markup="True"))
+            # Create semester summary labels
+            wam_sem_label = MDLabel(text="[b]WAM: -- --[/b]", markup=True,
+                                    height=dp(10), size_hint_y=None, text_size=(None, None), halign="center")
+            gpa4_sem_label = MDLabel(text="[b]GPA (4-point scale): --[/b]", markup=True,
+                                     height=dp(10), size_hint_y=None, text_size=(None, None), halign="center")
+            gpa7_sem_label = MDLabel(text="[b]GPA (7-point scale): --[/b]", markup=True,
+                                     height=dp(10), size_hint_y=None, text_size=(None, None), halign="center")
+
+            # Add them to the layout
+            semester_marks_section.add_widget(wam_sem_label)
+            semester_marks_section.add_widget(gpa4_sem_label)
+            semester_marks_section.add_widget(gpa7_sem_label)
+
+            # Store them for future access
+            self.semester_labels_dictionary[semester_label] = {
+                "wam": wam_sem_label,
+                "gpa4": gpa4_sem_label,
+                "gpa7": gpa7_sem_label
+            }
+            section_for_semester.add_widget(semester_marks_section)  # Add section to scroll widget for each semester
+
+            add_btn = MDRectangleFlatButton(  # Button to add subject to each semester
                 text="Add Subject",
                 size_hint=(None, None),
                 size=("140dp", "36dp"),
                 pos_hint={"center_x": 0.5},
-                on_release=lambda _, yl=year_label, sec=section_for_year: self.add_subject_row(yl, sec)
+                on_release=lambda _, sl=semester_label, sec=section_for_semester: self.add_subject_row(sl, sec)
             )
-            section_for_year.add_widget(add_btn)
+            section_for_semester.add_widget(add_btn)
 
-            self.root.ids.scroll_container.add_widget(section_for_year)  # Add current_section for each year to interface
+            self.root.ids.scroll_container.add_widget(section_for_semester)  # Add current_section for each semester to interface
 
-    def create_subject_row(self, parent, year_label):
-        # Function creates subject rows when value for years selected in dropdown
+    def create_subject_row(self, parent, semester_label):
+        # Function creates subject rows when value for semesters selected in dropdown
         row = MDBoxLayout(spacing=dp(10), size_hint_y=None, height=dp(48))  # Use boxlayout for each row for formatting
         black = (0, 0, 0, 1)
         row.add_widget(MDTextField(hint_text="Subject", mode="rectangle",
@@ -86,61 +125,80 @@ class MarksApp(MDApp):  # Class used to define backend logic
         row.add_widget(MDTextField(hint_text="Credit", input_filter="int", mode="rectangle", text="6",
                                    text_color_normal=black, text_color_focus=black))  # 3 textboxes for input
         bin_btn = MDIconButton(icon="trash-can", theme_text_color="Custom", text_color=(1, 0, 0, 1))
-        bin_btn.bind(on_release=lambda _, r=row: self.remove_subject_row(year_label, r, parent))  # Provide button functionality
+        bin_btn.bind(on_release=lambda _, r=row: self.remove_subject_row(semester_label, r, parent))  # Provide button functionality
         row.add_widget(bin_btn)
         return row
 
-    def add_subject_row(self, year_label, current_section):  # Functional called by add subject button
-        row = self.create_subject_row(current_section, year_label)  # Create a new row using predefined function
+    def add_subject_row(self, semester_label, current_section):  # Functional called by add subject button
+        row = self.create_subject_row(current_section, semester_label)  # Create a new row using predefined function
         current_section.add_widget(row, index=len(current_section.children) - 1)  # Insert row before Add button (the last index item)
-        self.subject_mark_dictionary[year_label].append(row)  # Add row to dictionary as well
+        self.subjects_marks_dictionary[semester_label].append(row)  # Add row to dictionary as well
         Clock.schedule_once(lambda dt: current_section.do_layout())  # Reformat the current section with new widget
         # Clock used to ensure this executes after the widget has been added
 
-    def remove_subject_row(self, year_label, row, section):
-        if row in self.subject_mark_dictionary[year_label]:
-            self.subject_mark_dictionary[year_label].remove(row)
+    def remove_subject_row(self, semester_label, row, section):
+        if row in self.subjects_marks_dictionary[semester_label]:
+            self.subjects_marks_dictionary[semester_label].remove(row)
             section.remove_widget(row)
             Clock.schedule_once(lambda dt: section.do_layout())
 
-    def calculate_gpa(self):  # Function calculate WAM and GPAs and adds to interface
-        total_weight = 0
-        total_credits = 0
-        for subject_sections in self.subject_mark_dictionary.values():
-            for subject in subject_sections:
+    def display_marks_to_interface(self):  # Function adds calculated WAMs and GPAs for degree and semesters to interface
+
+        def calculate_marks(total_credits, total_weight):  # Nested function calculates wam, gpa, grade and returns them
+            wam = round(total_weight / total_credits, 2) if total_credits else 0
+            if wam >= 85:
+                grade = 'HD'
+                gpa_7 = 7.0
+                gpa_4 = 4.0
+            elif 85 > wam >= 75:
+                grade = 'D'
+                gpa_7 = 6.0
+                gpa_4 = 3.5
+            elif 75 > wam >= 65:
+                grade = 'C'
+                gpa_7 = 5.0
+                gpa_4 = 3.0
+            elif 65 > wam >= 50:
+                grade = 'P'
+                gpa_7 = 4.0
+                gpa_4 = 2.0
+            else:
+                grade = 'F'
+                gpa_7 = 0.0
+                gpa_4 = 0.0
+
+            return wam, grade, gpa_4, gpa_7
+
+        # Initialise variables to pass to nested function calculate_marks() for calculation
+        degree_total_weight = 0
+        degree_total_credits = 0
+        for semester_name, semester_subjects in self.subjects_marks_dictionary.items():  # Loop over each semester
+            sem_weight = 0
+            sem_credits = 0
+            for subject in semester_subjects:  # Loop over each subject for the semester
                 try:
                     mark = float(subject.children[2].text)
                     credit = float(subject.children[1].text)
-                    total_weight += mark * credit
-                    total_credits += credit
-                except:  # try except used in case of invalid empty values (they are ignored)
-                    continue
-        wam = round(total_weight / total_credits, 2) if total_credits else 0
-        if wam >= 85:
-            grade = 'HD'
-            gpa_7 = 7.0
-            gpa_4 = 4.0
-        elif 85 > wam >= 75:
-            grade = 'D'
-            gpa_7 = 6.0
-            gpa_4 = 3.5
-        elif 75 > wam >= 65:
-            grade = 'C'
-            gpa_7 = 5.0
-            gpa_4 = 3.0
-        elif 65 > wam >= 50:
-            grade = 'P'
-            gpa_7 = 4.0
-            gpa_4 = 2.0
-        else:
-            grade = 'F'
-            gpa_7 = 0.0
-            gpa_4 = 0.0
 
-        # Add GPA and WAM to the interface
-        self.root.ids.wam_label.text = f"Predicted WAM: {wam} {grade}"
-        self.root.ids.gpa4_label.text = f"GPA (4-point scale): {gpa_4}"
-        self.root.ids.gpa7_label.text = f"GPA (7-point scale): {gpa_7}"
+                    degree_total_weight += mark * credit
+                    degree_total_credits += credit
+
+                    sem_weight += mark * credit
+                    sem_credits += credit
+                except:  # try except used in case of invalid empty values for a subject (subject is ignored)
+                    continue
+            # Section for calculating and adding semester marks to UI
+            sem_wam, sem_grade, sem_gpa4, sem_gpa7 = calculate_marks(sem_credits, sem_weight)
+            self.semester_labels_dictionary[semester_name]["wam"].text = f"WAM: {sem_wam} {sem_grade}"
+            self.semester_labels_dictionary[semester_name]["gpa4"].text = f"GPA (4-point scale): {sem_gpa4}"
+            self.semester_labels_dictionary[semester_name]["gpa7"].text = f"GPA (7-point scale): {sem_gpa7}"
+
+        degree_wam, degree_grade, degree_gpa4, degree_gpa7 = calculate_marks(degree_total_credits, degree_total_weight)
+
+        # Add degree GPA and WAM to the interface
+        self.root.ids.wam_label.text = f"WAM: {degree_wam} {degree_grade}"
+        self.root.ids.gpa4_label.text = f"GPA (4-point scale): {degree_gpa4}"
+        self.root.ids.gpa7_label.text = f"GPA (7-point scale): {degree_gpa7}"
 
 
 MarksApp().run()  # Main execution of class
